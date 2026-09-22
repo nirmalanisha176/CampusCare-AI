5e51cea5824d80d2179270511cd40233cf3d26c5
nirmalanisha176 <nirmalanisha176@gmail.com>
2026-09-22 09:58:29 +0530

    Add frontend React app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>CampusCare AI</h1>
+      <p>Intelligent Campus Emergency Assistance System</p>
+
+      <button>Report Emergency</button>
+    </div>
+  )
+}
+
+export default App
